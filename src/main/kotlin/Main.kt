0020eec87aa01b78559ebf564fef813b8b29b29c
Rida Salman <ridasalman458@gmail.com>
2023-05-31 16:44:15 +0500

    C-o commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -171,7 +171,7 @@ fun main(args: Array<String>){
 }  */
 //----------------------------------------------------------------------------------------------------------
 //-----------------------------Most IMP----------------------------------------------
-class smartDevice(){
+/*class smartDevice(){
     val name = "Android TV"
     val category = "Entertainment"
     var deviceStatus = "online"
@@ -188,16 +188,15 @@ fun main() {
     println("Device name is: ${sm.name}")
     sm.turnOn()
     sm.turnOff()
-}
+}*/
 //--------------------------------------------------------------------------------------------
-class Dog(
-    val name: String,
-    var weight: Int,
-    var height: Int,
-    val avgLifeExpectancy: Int) {
+/*class Dog (val name: String,
+           var weight: Int,
+           var height: Int,
+           val avgLifeExpectancy: Int){
 
-    fun speak() {
-        println("woof!")
+    fun speak(){
+        println("woof")
     }
 
     fun eat() {
@@ -209,13 +208,8 @@ class Dog(
     }
 }
 
-fun main() {
-    var myDog = Dog(
-        name="nora",
-        weight=10,
-        height=22,
-        avgLifeExpectancy=10)
-
+fun main(){
+    var myDog = Dog(name = "nora", weight = 10, height = 22, avgLifeExpectancy = 10)
     myDog.speak()
     myDog.eat()
     myDog.walk()
@@ -223,5 +217,221 @@ fun main() {
     println("Weight is ${myDog.weight}")
     println("Height is ${myDog.height}")
     println("Average life expectancy is ${myDog.avgLifeExpectancy}")
-}
+
+}*/
 //----------------------------------------------------------------------------------------------------------
+
+/*fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    // Fill in the code.
+    if(numberOfMessages < 100){
+        println("you have received $numberOfMessages notification")
+    }
+    else {
+        println("you have received 99+ notifications ")
+    }
+}*/
+//-------------------------------------------------------------------------------------------------------------
+/*Exercise 1: Create a Class
+Create a class called Person with the following properties:
+
+name (String): Represents the name of the person.
+age (Int): Represents the age of the person.
+Define a constructor to initialize the name and age properties.
+Exercise 2: Add a Method
+Add a method called introduce() to the Person class. The method should print a message introducing the person, including their name and age.
+
+Exercise 3: Create Objects
+Create two instances of the Person class and assign them to variables. Set different values for the name and age properties of each person.
+
+Exercise 4: Call Methods
+Call the introduce() method on both instances of the Person class to introduce each person.
+
+Exercise 5: Create a Class with Computed Property
+Create a class called Circle with the following properties:
+
+radius (Double): Represents the radius of the circle.
+Define a constructor to initialize the radius property.
+Add a computed property called area that calculates and returns the area of the circle (π * radius * radius).
+Exercise 6: Create an Object and Access Computed Property
+Create an instance of the Circle class and assign it to a variable. Set a value for the radius property of the circle object. Access and print the area property of the circle object.*/
+//-------------------------------------------------------------------------------------------------------
+/*class person(val name : String, val age : Int){
+    fun introduce(){
+        println("Hey my name is ${name}  and my age is ${age}  ")
+    }
+
+}
+class circle(var radius : Double){
+    val area: Double
+        get() = Math.PI*radius*radius
+
+}
+fun main(){
+    val p1 = person(name = "Rida", age = 21)
+    val p2 = person(name = "Nimra", age = 19)
+    val c1 = circle(4.5)
+    println(p1.introduce())
+    println(p2.introduce())
+    println("Radius : ${c1.radius}, Area : ${c1.area}")
+}*/
+//----------------------------------------------------------------------------------------------------------
+/*
+class car(val make : String, val model : String, val year : Int){
+    fun startEngine(){
+        println("Engine has Started $make $model")
+    }
+    fun stopEngine(){
+        println("Engine has Stopped $make $model")
+    }
+
+}
+class Rectangle(val length : Double, val width: Double ){
+    fun calculateArea(): Double{
+           return length * width
+    }
+
+}
+fun main(){
+    val c1 =  car(make = "Toyota", model = "Camry", year = 2018)
+    val c2 =  car(make = "Honda", model = "Civic", year = 2020)
+    val r1 = Rectangle(length = 4.5, width = 2.5)
+    val area = r1.calculateArea()
+    println("The area of the rectangle is: $area")
+    c1.stopEngine()
+    c2.startEngine()
+
+};*/
+//------------------------------------------------------------------------------------------
+
+/*class Student(val name: String, val age: Int) {
+      var grade: String = ""
+            get() {
+                  return field
+            }
+            set(value) {
+                  field = value
+            }
+}
+fun main(){
+      val student = Student("John", 20)
+      student.grade = "A" // Assigning "A" as the grade
+      println(student.grade) // Output: "A"
+}*/
+//------------------------------------------------------------------------------------------
+//this is an example of assigning properties without constructor
+/*class Mobile(){
+      var brand: String = ""
+      // model of type String
+      var model: String = ""
+      // mrp of type float
+      var mrp: Float = 0f
+      // discount of type float
+      var discount: Float = 0f
+      fun getActualPrice():Float{
+            return mrp - discount
+      }
+
+      fun printDetails(){
+            println("Mobile details:")
+            println("Brand: $brand")
+            println("Model: $model")
+            println("MRP: $mrp")
+            println("Discount: $discount")
+
+      }
+
+}*/
+/*fun main() {
+      val mobile = Mobile()
+      mobile.brand = "iPhone"
+      mobile.model = "11 pro"
+      mobile.mrp = 100000f
+      mobile.discount = 1000f
+
+      println("Discounted price is: ${mobile.getActualPrice()}")
+
+      mobile.printDetails()
+}*/
+//----------------------------------------------------------------------------------------------------------
+/*class Student (){
+      var ninja_id: Int = 0
+      var ninja_name: String = ""
+
+      fun add_details(ninja_id: Int, ninja_name: String){
+            this.ninja_id = ninja_id
+            this.ninja_name = ninja_name
+      }
+
+
+}
+fun main(){
+      var obj = Student()
+
+      obj.add_details(1, "Code Studio")
+
+      println("Ninja ID: ${obj.ninja_id}")
+      println("Ninja Name: ${obj.ninja_name}")
+}*/
+//-------------------------------------------------------------------------------------------------------------
+/*class Car() {
+    var model: String = ""
+    var engineNumber: Double = 0.0
+    var timetoMaxSpeed: Int = 0
+
+    fun reachMaxSpeedIn(distance: Int) {
+        println("Car reach to its Max Speed in ${distance * timetoMaxSpeed}")
+    }
+
+    fun printDetails() {
+        println("Car is of model :- $model having engine number $engineNumber")
+    }
+}
+
+class Student(val name: String, val age: Int) {
+    var grade: String = "0"
+        get() {
+            return when (field) {
+                in "100" -> "A"
+                in "80".."90" -> "B"
+                in "70".."80" -> "C"
+                in "60".."70" -> "D"
+                else -> "F"
+            }
+        }
+        set(value) {
+            if (value in "0".."100") {
+                field = value
+            } else {
+                println("Invalid grade! Grade should be between 0 and 100.")
+            }
+        }
+}
+
+fun String.ridaFullName(): String = "$this Salman"
+fun main() {
+
+      val student = Student("Rida", 50)
+      student.grade = "100"
+
+      val aasdf= 1
+      aasdf.toString()
+      println("Name: ${student.name.ridaFullName()} - Age: ${student.age} - Grade: ${student.grade}")
+//      val audi =  car()
+//      audi.model = "Audicode"
+//      audi.engineNumber = 395.90
+//      audi.timetoMaxSpeed = 30
+//
+//      audi.
+
+
+}*/
+//-----------------------------------THE END----------------------------------------------------------
